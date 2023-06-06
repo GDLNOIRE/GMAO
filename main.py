b30efbe694877e5f32048fa4d2a6d9bedaa19c6b
e2102021 <e2102021>
2023-06-06 12:00:33 +0200

    ajout notebook & merged tab & kilométrage classe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import graphviz
 import os
 from pyagrum_extra import gum
+import pyAgrum.lib.notebook as gnb 
 
 
 ot_odr_filename = os.path.join(".", "donnees/OT_ODR.csv.bz2")
@@ -11,6 +13,23 @@ ot_odr_df = pd.read_csv(ot_odr_filename,
 equipements_filename = os.path.join(".", 'donnees/EQUIPEMENTS.csv')
 equipements_df = pd.read_csv(equipements_filename,
                              sep=";")
+"""
+Transformation des kilométrages en classes
+"""
+
+# Définition des limites des intervalles pour chaque classe
+kilometrage_bins = [0, 15000, 30000, 60000, 85000, 115000,150000,200000,250000,float('inf')]
+
+# Définition des étiquettes pour chaque classe
+kilometrage_labels = ['0-15000', '15000-30000', '30000-60000', '60000-85000', '85000-115000', '115000-150000','150000-200000','200000-250000','250000+']
+
+# Transformation de la variable "kilométrage" en classe
+ot_odr_df['kilométrage_classe'] = pd.cut(ot_odr_df['kilométrage'], bins=kilometrage_bins, labels=kilometrage_labels)
+
+# Conversion de la nouvelle variable en catégorie
+ot_odr_df['kilométrage_classe'] = ot_odr_df['kilométrage_classe'].astype('category')
+print(ot_odr_df["kilométrage_classe"])
+
 
 #permet de décrire nos variables suivantes : 
 var_sig = ["SIG_ORGANE", "SIG_CONTEXTE", "SIG_OBS"]
@@ -51,20 +70,23 @@ bn.addArc("SIG_OBS", "SYSTEM_N1")
 import pyAgrum.lib.notebook as gnb 
 bn
 
-bn.cpt("SIG_OBS")
 bn.cpt("SYSTEM_N1")
-
+bn.cpt("SIG_OBS")
 bn.fit_bis(ot_odr_df, verbose_mode=True)
 bn.cpt("SIG_OBS")
 bn.cpt("SYSTEM_N1")
-
-pred_prob = bn.predict_proba(ot_odr_df[["SIG_OBS"]].iloc[-1000:], 
+pred_prob = bn.predict_proba(ot_odr_df[["SIG_OBS","SIG_ORGANE"]].iloc[-1000:], 
                              var_target="SYSTEM_N1",
                              show_progress=True)
 print(pred_prob)
 
-pred = bn.predict(ot_odr_df[["SIG_OBS"]].iloc[-1000:], 
+pred = bn.predict(ot_odr_df[["SIG_OBS","SIG_ORGANE"]].iloc[-1000:], 
                   var_target="SYSTEM_N1",
                   show_progress=True)
-
 print(pred)
+bn
+
+
+
+
+
